2a6102481d691b254932ffe05c056c5e61ba1467
hafizhdiaz <hafizhdiaz@gmail.com>
2019-09-10 18:20:30 +0700

    Tebak Angka FIX

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -1,7 +1,9 @@
 package id.ac.polinema.tebakangka;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,18 +29,82 @@ public class MainActivity extends AppCompatActivity {
 		initRandomNumber();
 	}
 
-	// TODO: generate angka random di sini
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "App on Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "App on Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "App on Restart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "App on Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "App on Destroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "App on Stop", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    // TODO: generate angka random di sini
 	private void initRandomNumber() {
 
         Random random = new Random();
-        int min=1;
-        int max=100;
-        randomNumber=random.nextInt(max - min) + min;
+        int min = 1;
+        int max = 100;
+        randomNumber = random.nextInt(max - min) + min;
 
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+        int input=Integer.parseInt(inputAngka.getText().toString());
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        if(input==randomNumber)
+        {
+            alertDialogBuilder.setMessage("Tebakan anda benar!");
+            btnTebak.setEnabled(false);
+        }
+        else if(input>randomNumber)
+        {
+            alertDialogBuilder.setMessage("Tebakan anda terlalu besar!");
+        }
+        else if(input<randomNumber)
+        {
+            alertDialogBuilder.setMessage("Tebakan anda terlalu kecil");
+        }
+
+        alertDialogBuilder.setNegativeButton("Tutup", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
 	}
 
 	public void handleReset(View view) {
